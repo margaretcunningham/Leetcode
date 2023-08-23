21ac0def09822a9ef2dc36595737a5caee72b41f
Margaret Cunningham <83427298+margaretcunningham@users.noreply.github.com>
2023-08-23 13:45:59 -0400

    Create problem160.cpp

---
+++ Easy/problem160.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        set<ListNode*> s;
+        while (headA != nullptr) {
+            s.insert(headA);
+            headA = headA->next;
+        }
+        while (headB != nullptr) {
+            if (s.count(headB) > 0) {
+                return headB;
+            }
+            headB = headB->next;
+        }
+        return headA;
+    }
+};
